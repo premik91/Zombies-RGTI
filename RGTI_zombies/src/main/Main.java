fd5b87273adf6c893dbe383e8b2b7e34af6d7146
Aljaz Prijatelj <aljaz.prijatelj@outfit7.com>
2012-11-21 09:02:06 +0100

    Removed some comments

--- RGTI_zombies/src/main/Main.java
+++ RGTI_zombies/src/main/Main.java
@@ -3,12 +3,10 @@ package main;
 public class Main {
 
 	public Main() {
-		// TODO Auto-generated constructor stub
 	}
 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 	}
 
 }
