644b02a6f8d7791ea579843599c5f9fe98c3d408
premik91 <i@premik91.com>
2012-11-26 14:15:47 +0100

    Making sure it is working

--- RGTI_zombies/src/main/Main.java
+++ RGTI_zombies/src/main/Main.java
@@ -1,12 +1,20 @@
 package main;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
 public class Main {
 
-	public Main() {
-	}
+    public Main() {
+        try {
+            Display.setDisplayMode(new DisplayMode(800, 600));
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        }
+    }
 
-
-	public static void main(String[] args) {
-	}
+    public static void main(String[] args) {
+        new Main();
+    }
 
 }
