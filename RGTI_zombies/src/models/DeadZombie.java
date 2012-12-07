2c63867db5574081c3c4386f873227e376367c42
premik91 <i@premik91.com>
2012-12-07 20:06:17 +0100

    Added dead zombies, static import, color terrain

--- RGTI_zombies/src/models/DeadZombie.java
+++ RGTI_zombies/src/models/DeadZombie.java
@@ -1,12 +1,15 @@
 package models;
+
 import static org.lwjgl.opengl.GL11.*;
 
-public class Terrain extends Model3D {
+public class DeadZombie extends Model3D {
+
+    public DeadZombie() {}
 
     @Override
     public void render() {
         glBegin(GL_QUADS);
-        glColor4f(0.5f, 0.5f, 0.4f, 1.0f);
+        glColor4f(1.0f, 0, 0, 1.0f);
         glVertex3f(1.0f, 0.0f, 1.0f);
         glVertex3f(-1.0f, 0.0f, 1.0f);
         glVertex3f(-1.0f, 0.0f, -1.0f);
